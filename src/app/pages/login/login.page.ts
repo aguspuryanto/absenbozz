dbb3b23707ccae0603d6b450f4334c033703c151
aguspuryanto <aguspuryanto@gmail.com>
2020-03-26 12:18:45 +0700

    update

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams, ToastController } from '@ionic/angular';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  credentials: any = {
+    username: "",
+    password: ""
+  }
 
-  constructor() { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidLoad() {
+    //check apakah sudah dalam kondisi login?
+    let status = localStorage.getItem("isLogin");
+    if (status == "true") {
+        // this.navCtrl.setRoot(HomePage);
+    }
+  }
+
+  login() {
+    this.auth.login(this.credentials).then((resp) => {
+        let toast = this.toastCtrl.create({
+            message: "Berhasil",
+            duration: 3000,
+            position: 'bottom'
+        });
+        // toast.present();
+        // this.navCtrl.setRoot(HomePage);
+    }).catch((err) => {
+        let toast = this.toastCtrl.create({
+            message: err,
+            duration: 2000,
+            position: 'bottom'
+        });
+        // toast.present();
+    })
   }
 
 }
